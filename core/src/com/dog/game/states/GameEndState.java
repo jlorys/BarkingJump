5499ede67b76b61c50f9d35eb25feb5785eb2276
Kuba <kuba35@vp.pl>
2019-11-10 11:54:08 +0100

    set accessibility to private

--- core/src/com/dog/game/states/GameEndState.java
+++ core/src/com/dog/game/states/GameEndState.java
@@ -83,7 +83,7 @@ class GameEndState extends State {
 
     }
 
-    public static float round(float d, int decimalPlace) {
+    private static float round(float d, int decimalPlace) {
         BigDecimal bd = new BigDecimal(Float.toString(d));
         bd = bd.setScale(decimalPlace, BigDecimal.ROUND_HALF_UP);
         return bd.floatValue();
